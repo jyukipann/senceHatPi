a64fe94b7a31bd887a664e843d8c30a8e1ca9273
JUKI <jyukipann@yahoo.co.jp>
2021-09-08 14:05:40 +0900

    print xyz

--- testAcc.py
+++ testAcc.py
@@ -5,3 +5,5 @@ acceleration = sense.get_accelerometer_raw()
 x = str( round( acceleration['x'],1 ) )
 y = str( round( acceleration['y'],1 ) )
 z = str( round( acceleration['z'],1 ) )
+
+print(x,y,z)
