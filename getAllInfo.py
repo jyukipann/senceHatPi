e3c3dcde3f1caf5be46fc66ef7b4af11513004e9
JUKI <jyukipann@yahoo.co.jp>
2021-09-10 11:42:45 +0900

    add

--- getAllInfo.py
+++ getAllInfo.py
@@ -1,15 +1,43 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 import time
+import math
+
+#calib
+#重力加速度g
+G = 9.80665
+#加速度センサの誤差ノルム
+gs_delta = 0.975374592
+
+#last current
+t_l_c = [0,0]
+a_l_c = [0,0]
+v_l_c = [0,0]
+x_l_c = [0,0]
+
+def norm(x,y,z):
+	return math.sqrt(x**2 + y**2+ z**2)
+
+def trapezoidal(last, current, dt):
+	return ((last + current)/2)*dt
 
 def getAllInfo():
-	global sense
+	global sense,t_l_c,a_l_c,v_l_c,x_l_c,G,gs_delta
 	#[time,accel,gyro,mag,temp,humidty,pressure]
 	ac = sense.accel_raw
 	gy = sense.gyro_raw
 	co = sense.compass_raw
+	t_l_c[0] = t_l_c[1]
+	t_l_c[1] = time.time()
+	dt = t_l_c[1] - t_l_c[0]
+	a_l_c[0] = a_l_c[1]
+	a_l_c[1] = (norm(*ac.values())-gs_delta)*G
+	v_l_c[0] = v_l_c[1]
+	v_l_c[1] += trapezoidal(*a_l_c, dt)
+	x_l_c[0] = x_l_c[1]
+	x_l_c[1] += trapezoidal(*v_l_c, dt)
 	return {
-		"time":time.time(),
+		"time":t_l_c[1],
 		"accel_row_x":ac["x"],
 		"accel_row_y":ac["y"],
 		"accel_row_z":ac["z"],
@@ -21,6 +49,8 @@ def getAllInfo():
 		"compass_raw_z":co["z"],
 		"temp":sense.temp,
 		"pressure":sense.pressure,
+		"velocity":v_l_c[1],
+		"displacement":x_l_c[1],
 	}
 
 
